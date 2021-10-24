02e1db9a034195092431607d44677f73a19cfa4c
Max Radin <radin.max@gmail.com>
2021-10-24 16:18:54 -0400

    Attempting to add testing action

---
+++ .github/workflows/run_tests.m
@@ -0,0 +1,10 @@
+% Run all unit tests.
+
+addpath("../..")
+objects = dir("../../tests");
+for i = 1:numel(objects)
+    if regexp(objects(i).name, ".*\.m")
+        fprintf("Running %s\n", objects(i).name)
+        run(fullfile(objects(i).folder, objects(i).name))
+    end
+end
